c075f8c7aec6596810ae655f15e7a87905a0ba19
Elkana Rop <elkanarop@gmail.com>
2017-09-26 01:12:15 +0000

    Commit send email feature

--- work_with_dict.py
+++ work_with_dict.py
@@ -1,3 +1,6 @@
+import smtplib
+
+
 class Event:
 
 	def __init__(self):
@@ -13,12 +16,29 @@ class Event:
 		name = input("Enter name to search: ")
 		for key in self.contacts_dict:
 			if key == name:
-				print(self.contacts_dict[key]) 
+				print(self.contacts_dict[key])
+
+	def send_email(self):
+		print("Your email is: {}".format(self.email))
+		self.pswrd = input("Enter your pasword: ")
+		self.reciver_email = input("Thankyou, now enter email for reciver: ")
+		self.message = input("Enter message: ") 
+		return self.message
+
 
 
 mest_party = Event()
 mest_party.register_person()
 mest_party.search_contact()
+mest_party.send_email()
+
+mail = smtplib.SMTP('smtp.gmail.com',587)
+mail.ehlo()
+mail.starttls()
+mail.login(mest_party.email,mest_party.pswrd)
+mail.sendmail(mest_party.email,mest_party.reciver_email,mest_party.message)
+mail.close()
+print("Succesfully sent :)")
 
 
 
